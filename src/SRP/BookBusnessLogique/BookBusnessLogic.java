7e2723db6ebce69c1a6ca306a4dd09410b98da93
fabiola-0405 <tassolimofabiola@gmail.com>
2025-12-24 10:57:51 +0100

    implementation du SRP avec refactoring

---
+++ src/SRP/BookBusnessLogique/BookBusnessLogic.java
@@ -0,0 +1,18 @@
+package SRP.BookBusnessLogique;
+
+import SRP.Solution.BookSRP;
+
+         class BookBusinessLogic {
+
+             public void emprunter(BookSRP book, String lecteur) {
+
+                 System.out.println("\nEmprunt du livre '" + book.getTitle() + "' par " + lecteur);
+             }
+
+//On peut ajouter d'autres logiques
+
+             public void autreService(BookSRP book) {
+
+                 System.out.println("\nAutre logique métier sur le livre '" + book.getTitle());
+             }
+         }
---
+++ src/SRP/BookSave/BookSave.java
@@ -0,0 +1,17 @@
+package SRP.BookSave;
+
+import SRP.Solution.BookSRP;
+
+// Classe BookSaver : persistance
+ class BookSaver {
+
+    public void saveToDatabase(BookSRP book) {
+
+        System.out.println("\nSauvegarde de '" + book.getTitle() + "' en base de données..."); }
+
+//On peut ajouter d'autres façons de sauvegarder
+
+    public void saveToFile(BookSRP book, String filename) {
+
+        System.out.println("\nSauvegarde de '" + book.getTitle() + "' dans " + filename); }
+ }
